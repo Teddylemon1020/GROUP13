02f1d8cd5cb1e94185f541331c6568e18de5d7be
Teddylemon1020 <hakkiroan@gmail.com>
2025-10-08 21:39:24 +0800

    feat: google signin

---
+++ Eventura/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/utils/mongodbclient";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
