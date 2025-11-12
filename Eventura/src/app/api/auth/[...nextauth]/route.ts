1dacf54007dfb9c9d4c55b0f1ef4a8ca1b6e8062
Teddylemon1020 <hakkiroan@gmail.com>
2025-11-12 10:09:41 +0800

    fix: deployment regulations

--- Eventura/src/app/api/auth/[...nextauth]/route.ts
+++ Eventura/src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,5 @@
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/utils/mongodbclient";
-import NextAuth, { AuthOptions, Session, Account, Profile } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
-
-export const authOptions: AuthOptions = {
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true, // Allows linking OAuth to existing email accounts
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/signup",
-  },
-  callbacks: {
-    async jwt({ token, account, profile }: { token: JWT; account: Account | null; profile?: Profile }) {
-      if (account && profile) {
-        token.accessToken = account.access_token;
-        token.id = profile.sub;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
